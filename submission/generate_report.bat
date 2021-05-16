42558c093b18f1f80175c62918f04fda9912c627
Frinze Erin <frinzekt@gmail.com>
2021-05-16 18:44:18 +0800

    fix stuff

--- submission/generate_report.bat
+++ submission/generate_report.bat
@@ -1,3 +1,5 @@
-jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to latex
-jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to pdf
-jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to html
+jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --to html --output wo_input.html
+jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to html --output wo_output.html
+jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to html --output complete.html
+@REM jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to latex
+@REM jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to pdf
