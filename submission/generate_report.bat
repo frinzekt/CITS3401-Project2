96209caac94c6ff412ae7cb1f71eee4035ae04e8
Max Matthews <21506225@student.uwa.edu.au>
2021-05-16 18:14:27 +0800

    generate report - semi ready

--- submission/generate_report.bat
+++ submission/generate_report.bat
@@ -1,3 +1,3 @@
-jupyter nbconvert project.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to latex
-jupyter nbconvert project.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to pdf
-jupyter nbconvert project.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to html
+jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to latex
+jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to pdf
+jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to html
