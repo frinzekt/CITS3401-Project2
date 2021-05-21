16e61e2a50d352e219dbdd26aa48ebd42249a0bb
Frinze Erin <frinzekt@gmail.com>
2021-05-21 20:02:17 +0800

    state of report

--- submission/generate_report.bat
+++ submission/generate_report.bat
@@ -4,4 +4,4 @@ jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_all_o
 jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to latex
 @REM jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to latex
 jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to pdf
-@REM jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to pdf
+jupyter nbconvert project_with_report.ipynb --TagRemovePreprocessor.remove_cell_tags="{'remove_input'}" --TagRemovePreprocessor.remove_all_outputs_tags="{'remove_output'}" --to pdf  --output alternate.pdf
